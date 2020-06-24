29e74eca4c16837bc3bfa74baf29ec13f69d9c8b
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2020-06-24 16:52:53 +0900

    updated index.html

--- src/canvas.ts
+++ src/canvas.ts
@@ -5,3 +5,9 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
+
+ctx.fillStyle = "rgb(200,0,0)";
+ctx.fillRect (10, 10, 50, 50);
+
+ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+ctx.fillRect (30, 30, 50, 50);
